8a7c15459323d9322781e3177da69ed29b090611
Edmund <esr360@live.com>
2015-08-01 19:47:42 +0100

    adding owl carousel in place of slick

--- assets/js/global.js
+++ assets/js/global.js
@@ -111,14 +111,80 @@ window.sr = new scrollReveal();
 // Carousels
 //-----------------------------------------------------------------
 
-$('#showcase-thumbnails').owlCarousel({
-	items: 5
-});
+// Showcase Thumbnails Carousel
+//-----------------------------------------------------------------
 
-$('#showcase-carousel').owlCarousel({
+var $sync1 = $("#showcase-carousel"),
+	$sync2 = $("#showcase-thumbnails"),
+	flag = false,
+	duration = 300;
+
+// Showcase Slides
+$sync1.owlCarousel({
 	items: 1
+})
+.on('changed.owl.carousel', function (e) {
+	if (!flag) {
+		flag = true;
+		$sync2.trigger('to.owl.carousel', [e.item.index, duration, true]);
+		flag = false;
+	}
+})
+.trigger('to.owl.carousel', 2);
+
+// Showcase Thumbs
+$sync2.owlCarousel({
+    center: true,
+    items: 5,
+	stagePadding: 0,
+    responsive: {
+        600: {
+            items:4
+        }
+    }
+})
+.on('click', '.owl-item', function () {
+	$sync1.trigger('to.owl.carousel', [$(this).index(), duration, true]);
+})
+.on('changed.owl.carousel', function (e) {
+	if (!flag) {
+		flag = true;		
+		$sync1.trigger('to.owl.carousel', [e.item.index, duration, true]);
+		flag = false;
+	}
+})
+.trigger('to.owl.carousel', 2);
+
+// Slide Arrows
+
+$sync2.find("+ .wrap-slide-arrows .slide-next").click(function() {
+    $sync2.trigger('next.owl.carousel');
+});
+$sync2.find("+ .wrap-slide-arrows .slide-prev").click(function() {
+    $sync2.trigger('prev.owl.carousel');
 });
 
-$('#shortcodes-carousel').owlCarousel();
+// Shortcodes Carousel
+//-----------------------------------------------------------------
+
+var shortcodesCarousel = $("#shortcodes-carousel");
+
+shortcodesCarousel.owlCarousel({
+    items: 3,
+	center: true,
+	loop: true
+})
+.on('click', '.owl-item', function(e) {
+	var carousel = shortcodesCarousel.data('owl.carousel');
+	e.preventDefault();
+	carousel.to(carousel.relative($(this).index()));
+});
+
+$("#shortcodes .slide-next").click(function() {
+    shortcodesCarousel.trigger('next.owl.carousel');
+});
+$("#shortcodes .slide-prev").click(function() {
+    shortcodesCarousel.trigger('prev.owl.carousel');
+});
 
 }); // end document.ready
---
+++ assets/js/owl-carousel/owl-carousel.js
@@ -0,0 +1,8 @@
+//@prepros-append owl.carousel.js
+//@prepros-append owl.animate.js
+//@prepros-append owl.autoheight.js
+//@prepros-append owl.autoplay.js
+//@prepros-append owl.hash.js
+//@prepros-append owl.lazyload.js
+//@prepros-append owl.navigation.js
+//@prepros-append owl.video.js
