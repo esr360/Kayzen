d7d3156365e735f5053ad6221774b27b5637e329
Edmund <esr360@live.com>
2015-08-23 18:59:28 +0100

    updating architecture

---
+++ assets/app.js
@@ -0,0 +1,47 @@
+/******************************************************************
+
+Kayzen
+Made by @esr360
+
+******************************************************************/
+
+//-----------------------------------------------------------------
+// Plugins
+//-----------------------------------------------------------------
+
+//@prepros-append plugins/animate-numbers.js
+//@prepros-append plugins/scroll-reveal.js
+
+//-----------------------------------------------------------------
+// Utilities
+//-----------------------------------------------------------------
+
+//@prepros-append utilities/adaptive-columns.js
+//@prepros-append utilities/tools.js
+//@prepros-append vendor/Modular/modular.js
+
+//-----------------------------------------------------------------
+// Elements
+//-----------------------------------------------------------------
+
+//@prepros-append modules/elements/accordions/accordions.js
+//@prepros-append modules/elements/carousels/carousels.js
+//@prepros-append modules/elements/modals/modals.js
+//@prepros-append modules/elements/progress-bars/progress-bars.js
+//@prepros-append modules/elements/tabs/tabs.js
+//@prepros-append modules/elements/tooltips/tooltips.js
+
+//-----------------------------------------------------------------
+// Objects
+//-----------------------------------------------------------------
+
+//@prepros-append modules/objects/google-map/google-map.js
+//@prepros-append modules/objects/header/header.js
+//@prepros-append modules/objects/navigation/flyout-navigation.js
+//@prepros-append modules/objects/scroll-top/scroll-top.js
+
+//-----------------------------------------------------------------
+// Theme
+//-----------------------------------------------------------------
+
+//@prepros-append themes/Kayzen/kayzen.js
