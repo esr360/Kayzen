0ce4c1974efa6890c1117868ffa67c2ade441ff8
Edmund <esr360@live.com>
2015-09-04 23:45:42 +0100

    lisr related updates

--- app/compiled/scripts/app.js
+++ app/compiled/scripts/app.js
@@ -4787,16 +4787,6 @@ if (setting('app-header', 'side')) {
 ;//=================================================================
 // Dropdown
 //=================================================================
-
-if (setting('dropdown', 'lock-screen')) {
-	$(navigation).find('> ul > li > ul').on('mouseenter', function () {
-		$(document).bind('mousewheel DOMMouseScroll',function(){ 
-			stopWheel(); 
-		});
-	}).on('mouseleave', function () {
-		$(document).unbind('mousewheel DOMMouseScroll');
-	});
-}
 ;
 /* Flyout Navigation
 ================================================================ */
--- app/modules/objects/dropdown/dropdown.js
+++ app/modules/objects/dropdown/dropdown.js
@@ -1,13 +1,3 @@
 //=================================================================
 // Dropdown
 //=================================================================
-
-if (setting('dropdown', 'lock-screen')) {
-	$(navigation).find('> ul > li > ul').on('mouseenter', function () {
-		$(document).bind('mousewheel DOMMouseScroll',function(){ 
-			stopWheel(); 
-		});
-	}).on('mouseleave', function () {
-		$(document).unbind('mousewheel DOMMouseScroll');
-	});
-}
