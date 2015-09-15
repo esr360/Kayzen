e09fee0041f7205b716c5d077e47155bb4c379a7
Edmund Reed <esr360@live.com>
2015-09-15 01:21:28 +0100

    various updates

--- app/modules/objects/top-bar/top-bar.php
+++ app/modules/objects/top-bar/top-bar.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<div class="row-block">
 			
-			<div class="row span">
-				<div class="icon_group-small span va-middle">
+			<div class="top-bar_item row span">
+				<div class="icon_group-small span">
 					<a href="#" class="icon-border-size-2">
 						<i class="fa fa-facebook"></i>
 					</a>
@@ -17,26 +17,45 @@
 						<i class="fa fa-google-plus"></i>
 					</a>
 				</div>
-				<div class="span va-middle">
+				<div class="span">
 					<small><a href="#"><i class="fa fa-envelope-o"></i> support@kayzen.com</a></small>
 				</div>
-				<div class="span va-middle">
+				<div class="span">
 					<small><a href="#"><i class="fa fa-phone"></i> (310) 276-2251</a></small>
 				</div>
 			</div>
 			
 			<div class="span text-right">
-				<div class="button_group span va-middle text-right">
+				
+				<div class="button_group span va-middle">
 					<a href="#" class="button-size-1">
 						<i class="fa fa-user"></i> Login
 					</a>
 					<a href="#" class="button-primary-size-1">
 						<i class="fa fa-pencil-square-o"></i> Register
 					</a>
-					<a href="#" class="icon-plain-size-3">
-						<i class="fa fa-search"></i>
-					</a>
 				</div>
+				
+				<div class="top-bar_nav span va-middle">
+					<ul>
+						<li>
+							<a href="#">
+								<i class="fa fa-search"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" data-notification="3">
+								<i class="fa fa-shopping-cart"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<small>ENG</small> <i class="fa fa-angle-down"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+				
 			</div>
 			
 		</div><!-- row-block -->
