c5e8920939eb0cc8609246baffab96474589b347
Edmund Reed <esr360@live.com>
2015-09-18 01:23:15 +0100

    collapsible side nav dropdown

--- app/compiled/scripts/app.js
+++ app/compiled/scripts/app.js
@@ -5555,6 +5555,30 @@ if (setting('app-header', 'side')) {
 	
 	$(appHeader).prependTo('body');
 	
+	// create open/close icon
+	var openClose = '<i class="side-nav_openClose fa ' + module['side-nav']['collapsible']['icon'] + '"></i>'
+	
+	// add collapsible functionality
+	if (setting('side-nav', 'collapsible')) {
+		$(navigation).find('a:not(:only-child)').prepend(openClose);
+		$(navigation)
+			.find('li > [class*="mega-menu"]').parent()
+			.find('.side-nav_openClose').remove();
+		$('.side-nav_openClose').each(function() {
+			$(this).click(function(){
+				console.log('test');
+				$(this).parent().find('~ ul').slideToggle(baseTransition);
+			});
+		});
+	}
+	
+	// collapse by default
+	var setting = module['side-nav']['collapsible']['open-by-default'];
+	if ($(navigation).is('[class*="-collapse"]') == true || setting == false) {
+		$(navigation).find('a:not(:only-child) ~ ul').hide();
+	}
+	
+	
 }
 ;//=================================================================
 // Dropdown
