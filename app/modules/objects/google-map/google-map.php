b230df3930d468f6d021d1644617a326e0550313
Edmund <esr360@live.com>
2015-10-13 23:52:59 +0100

    removing sass-cache files from repo

--- app/modules/objects/google-map/google-map.php
+++
@@ -1,5 +0,0 @@
-<!-- Google Map -->
-<div class="google-map" id="map"></div>
-
-<script src="<?php echo appDir ?>/modules/objects/google-map/google-map.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
--- app/vendor/Owl-Carousel/owl-carousel.js
+++
@@ -1,8 +0,0 @@
-//@prepros-append js/owl.carousel.js
-//@prepros-append js/owl.animate.js
-//@prepros-append js/owl.autoheight.js
-//@prepros-append js/owl.autoplay.js
-//@prepros-append js/owl.hash.js
-//@prepros-append js/owl.lazyload.js
-//@prepros-append js/owl.navigation.js
-//@prepros-append js/owl.video.js
