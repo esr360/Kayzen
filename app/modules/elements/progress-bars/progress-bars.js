3d3b343111a17d2b032a95a0354da7df2baadb93
Edmund <esr360@live.com>
2015-10-13 20:00:23 +0100

    gitignore is now working

--- app/modules/elements/progress-bars/progress-bars.js
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------
-// Progress Bars
-//-----------------------------------------------------------------
-
-$("progress.progress-bar").each(function() {
-    var attrProgress = $(this).attr('data-progress');
-    $(this).attr('value', attrProgress.replace(/[^-\d\.]/g, ''));
-    $(this).find('.progress').css({ width : attrProgress }); 
-});
