3cd47f4446972e8fbea28ec980fa20bbd85f4f98
Edmund <esr360@live.com>
2015-10-13 19:57:28 +0100

    updating grunt files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,58 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
     grunt.initConfig({
 		
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-        }
+		concat: {   
+			dist: {
+				src: [
+					// Vendor					
+					'app/vendor/Modular/modular.js',
+					'app/vendor/Owl-Carousel/owl-carousel.js',
+					
+					// Plugins					
+					'app/plugins/*.js',
+					
+					// Includes
+					'app/includes/*.js',
+					
+					// Elements
+					'app/modules/elements/*/*.js',
+					
+					// Objects					
+					'app/modules/objects/*/*.js'
+					
+					// Themes
+				],
+				dest: 'app/compiled/scripts/app.js',
+			}
+		},
+		
+		uglify: {
+			build: {
+				src: 'app/compiled/scripts/app.js',
+				dest: 'app/compiled/scripts/app.min.js'
+			}
+		},
+		
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'app/images/build/'
+				}]
+			}
+		}
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
 
 };
