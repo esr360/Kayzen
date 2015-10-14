5e747b7c1048c950619034be06d14568d04f2b03
Edmund Reed <esr360@live.com>
2015-10-14 23:24:49 +0100

    messing with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,10 @@ module.exports = function(grunt) {
             build: {
                 src: 'app/compiled/scripts/app.js',
                 dest: 'app/compiled/scripts/app.min.js'
+            },
+            Modernizr: {
+                src: 'app/vendor/Modernizr/modernizr.js',
+                dest: 'app/compiled/scripts/modernizr.min.js'
             }
         },
         
@@ -62,14 +66,23 @@ module.exports = function(grunt) {
                     spawn: false,
                 }
             }
+        },
+        
+        run_grunt: {
+            Modernizr: {
+                //task: ['clean', 'jshint:strictTarget', 'mocha:subTarget'],
+                //src: ['app/vendor/Modernizr/Gruntfile.js']
+            }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-modernizr");
+    grunt.loadNpmTasks('grunt-run-grunt');
 
     grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
 
