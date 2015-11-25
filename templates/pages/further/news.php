1bc103d593a85c9a9e813006fe5eee2747b8940e
Edmund Reed <esr360@live.com>
2015-11-25 20:36:01 +0000

    final template!!!

---
+++ templates/pages/further/news.php
@@ -0,0 +1,463 @@
+<?php 
+	include ('../../app.php');
+	// page config
+	$title = 'Keep Upto Date';
+	$tagLine = 'Aenean lobortis ante nunc urabitur at enim nisi.';
+?>
+
+<!DOCTYPE html>
+<html class="no-js">
+
+<?php include (ROOT.'/includes/head.php'); ?>
+
+<body>
+
+    <!-- Site Canvas -->
+    <div id="site-content">
+		
+		<?php 
+        
+            topbar();
+
+            appHeader(array(
+                'modifiers' => 'bar-absolute-dark-sticky'
+            ));
+
+            billboard(array(
+                'title'    => $title,
+                'tag-line' => $tagLine
+            ));
+            
+        ?>
+		
+		<section class="section-primary">
+			
+			<div class="container">		
+				
+				<div class="row">
+					
+					<div class="newsfeed span-8">
+							
+						<div class="relative">
+								
+							<!-- Latest Articles -->
+							
+							<div class="owl-carousel" id="latest-articles">
+								
+								<div class="thumbnail-feature">
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-2.jpg">
+									<div class="thumbnail_content">
+										<header class="heading_group-small">
+											<div class="heading_date">
+												<div>Sep</div>
+												<div>23</div>
+											</div>
+											<span>
+												<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+											</span>
+										</header>
+										<div class="thumbnail_excerpt">Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...</div>
+									</div>
+								</div>
+								
+								<div class="thumbnail-feature">
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-3.jpg">
+									<div class="thumbnail_content">
+										<header class="heading_group-small">
+											<div class="heading_date">
+												<div>Sep</div>
+												<div>23</div>
+											</div>
+											<span>
+												<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+											</span>
+										</header>
+										<div class="thumbnail_excerpt">Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...</div>
+									</div>
+								</div>
+								
+								<div class="thumbnail-feature">
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-5.jpg">
+									<div class="thumbnail_content">
+										<header class="heading_group-small">
+											<div class="heading_date">
+												<div>Sep</div>
+												<div>23</div>
+											</div>
+											<span>
+												<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+											</span>
+										</header>
+										<div class="thumbnail_excerpt">Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...</div>
+									</div>
+								</div>
+								
+								<div class="thumbnail-feature">
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-6.jpg">
+									<div class="thumbnail_content">
+										<header class="heading_group-small">
+											<div class="heading_date">
+												<div>Sep</div>
+												<div>23</div>
+											</div>
+											<span>
+												<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+											</span>
+										</header>
+										<div class="thumbnail_excerpt">Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...</div>
+									</div>
+								</div>
+								
+								<div class="thumbnail-feature">
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-7.jpg">
+									<div class="thumbnail_content">
+										<header class="heading_group-small">
+											<div class="heading_date">
+												<div>Sep</div>
+												<div>23</div>
+											</div>
+											<span>
+												<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+											</span>
+										</header>
+										<div class="thumbnail_excerpt">Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...</div>
+									</div>
+								</div>
+								
+							</div>
+							
+							<nav class="thumbnail_nav slide-nav" id="latest-articles-nav">
+								<button class="slide-prev icon-size-5"><i class="fa fa-angle-left"></i></button>
+								<button class="slide-next icon-size-5"><i class="fa fa-angle-right"></i></button>
+							</nav>
+						
+						</div>
+						
+						<div class="thumbnail_grid-fade row-magic" id="latest-articles-thumbs">
+							<img class="span active" src="<?php echo appDir ?>/images/wallpapers/bg-2.jpg">
+							<img class="span" src="<?php echo appDir ?>/images/wallpapers/bg-3.jpg">
+							<img class="span" src="<?php echo appDir ?>/images/wallpapers/bg-5.jpg">
+							<img class="span" src="<?php echo appDir ?>/images/wallpapers/bg-6.jpg">
+							<img class="span" src="<?php echo appDir ?>/images/wallpapers/bg-7.jpg">
+						</div>
+						
+						<script>
+							$(document).ready(function() {
+                                
+                                $("#latest-articles").kCarousel({
+                                    navId: '#latest-articles-nav',
+                                    pagerId: '#latest-articles-thumbs',
+                                    pagerSelector: 'img'
+                                });
+								
+							});
+						</script>
+						
+						<hr class="hrule-stripes-large" />
+								
+						<!-- Featured Articles -->
+								
+						<div class="row">	
+							
+							<div class="span-6 relative">	
+								<div class="thumbnail-zoom">
+									<div class="thumbnail_mask">
+										<div class="icon_group-small">
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-search"></i>
+											</a>
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-link"></i>
+											</a>
+										</div>
+									</div>
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-7.jpg">
+								</div>
+								<div class="thumbnail-profile-round-badge">
+									<img src="<?php echo appDir ?>/images/team/team-2.jpg" alt="">
+								</div>
+								<header class="heading_group">
+									<div class="heading_date">
+										<div>Sep</div>
+										<div>23</div>
+									</div>
+									<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+									<h4 class="heading-light-size-3">Posted by <a href="#">Naomi Olson</a></h4>
+								</header>
+								<p>Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec finibus fringilla purus. Integer libero justo, mattis et magna eget...</p>
+								<div class="row">
+									<div class="span-6 va-middle">
+										<a href="#" class="button-oval-primary">Read More</a>
+									</div>
+									<div class="span-6 va-middle text-right">
+										<small>Posted in <a href="blog.html">Web Design</a></small>
+									</div>
+								</div>
+							</div>
+							
+							<div class="span-6 relative">	
+								<div class="thumbnail-zoom">
+									<div class="thumbnail_mask">
+										<div class="icon_group-small">
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-search"></i>
+											</a>
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-link"></i>
+											</a>
+										</div>
+									</div>
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-5.jpg">
+								</div>
+								<div class="thumbnail-profile-round-badge">
+									<img src="<?php echo appDir ?>/images/team/team-2.jpg" alt="">
+								</div>
+								<header class="heading_group">
+									<div class="heading_date">
+										<div>Sep</div>
+										<div>23</div>
+									</div>
+									<h3 class="heading-heavy-size-4">Kayzen is Released</h3>
+									<h4 class="heading-light-size-3">Posted by <a href="#">Naomi Olson</a></h4>
+								</header>
+								<p>Cum socis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec finibus fringilla purus. Integer libero justo, mattis et magna eget...</p>
+								<div class="row">
+									<div class="span-6 va-middle">
+										<a href="#" class="button-oval-primary">Read More</a>
+									</div>
+									<div class="span-6 va-middle text-right">
+										<small>Posted in <a href="blog.html">Web Design</a></small>
+									</div>
+								</div>
+							</div>
+							
+						</div><!-- row -->
+						
+						<hr class="hrule-stripes-large" />
+								
+						<!-- Featured Categories -->
+						
+						<div class="widget_group row-magic text-center">
+							
+							<div class="widget-borderTop-highlight-block-scale span">
+								<div class="widget_icon icon-plain-size-9">
+									<i class="fa fa-desktop"></i>
+								</div>
+								<header class="widget_header heading_group">
+									<h3 class="heading-heavy-size-4">100% Responsive</h3>
+									<h4 class="heading-light-uppercase-brand-1">Donec finibus fringer</h4>
+								</header>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et erat posuere suscipit.</p>
+							</div>
+							
+							<div class="widget-borderTop-highlight-block-scale span">
+								<div class="widget_icon icon-plain-size-9">
+									<i class="fa fa-desktop"></i>
+								</div>
+								<header class="widget_header heading_group">
+									<h3 class="heading-heavy-size-4">100% Responsive</h3>
+									<h4 class="heading-light-uppercase-brand-1">Donec finibus fringer</h4>
+								</header>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et erat posuere suscipit.</p>
+							</div>
+							
+							<div class="widget-borderTop-highlight-block-scale span">
+								<div class="widget_icon icon-plain-size-9">
+									<i class="fa fa-desktop"></i>
+								</div>
+								<header class="widget_header heading_group">
+									<h3 class="heading-heavy-size-4">100% Responsive</h3>
+									<h4 class="heading-light-uppercase-brand-1">Donec finibus fringer</h4>
+								</header>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et erat posuere suscipit.</p>
+							</div>
+							
+						</div>
+						
+						<hr class="hrule-stripes-large" />
+								
+						<!-- Featured Comments -->
+						
+						<section 
+							class="section-secondary-mini-overlay-alt"
+							style="background-image: url('<?php echo appDir ?>/images/polygon-bg-blue.jpg')"
+						>
+						
+							<div class="container text-center owl-carousel force-dots" id="testimonials-carousel">
+								
+								<blockquote class="blockquote">
+									<a class="blockquote_avatar-large" href="#">
+										<img src="/app/images/team/team-2.jpg" alt="">
+									</a>
+									<div class="blockquote_content">
+										<p>Suspendisse tempus sodales neque, eget eleifend <b>turpis tristique</b> eu. Nullam a nisl maximus, <b>ultrices est</b> ut blandit nislr, elit in lobortis mattis.</p>
+									</div>
+									<div class="blockquote_cite"> 
+										<h4 class="heading-highlight-light"><b>Brendon Mack - Theme Forest</b></h4>
+									</div>
+								</blockquote>
+								
+								<blockquote class="blockquote">
+									<a class="blockquote_avatar-large" href="#">
+										<img src="/app/images/team/team-2.jpg" alt="">
+									</a>
+									<div class="blockquote_content">
+										<p>Suspendisse tempus sodales neque, eget eleifend <b>turpis tristique</b> eu. Nullam a nisl maximus, <b>ultrices est</b> ut blandit nislr, elit in lobortis mattis.</p>
+									</div>
+									<div class="blockquote_cite"> 
+										<h4 class="heading-highlight-light"><b>Brendon Mack - Theme Forest</b></h4>
+									</div>
+								</blockquote>
+								
+							</div>
+							
+							<script>
+								$(document).ready(function() {
+									
+									var testimonialsCarousel = $("#testimonials-carousel");
+									
+									testimonialsCarousel.owlCarousel({
+										items: 1,
+										margin: 30
+									})
+									
+								});
+							</script>
+							
+						</section>
+						
+						<hr class="hrule-stripes-large" />
+								
+						<!-- More Articles -->
+						
+						<div class="row">
+							
+							<div class="span-4 relative">	
+								<div class="thumbnail-zoom">
+									<div class="thumbnail_mask">
+										<div class="icon_group-small">
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-search"></i>
+											</a>
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-link"></i>
+											</a>
+										</div>
+									</div>
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-7.jpg">
+								</div>
+								<header class="heading_group">
+									<div class="heading_date">
+										<div>Sep</div>
+										<div>23</div>
+									</div>
+									<h3 class="heading-heavy-size-3">Kayzen is Released</h3>
+									<h4 class="heading-light">Posted by <a href="#">Naomi Olson</a></h4>
+								</header>
+								<p>Cum socis natoque penatibus et magnis dis parturient montes...</p>
+								<div class="row">
+									<div class="span-5 va-middle">
+										<a href="#" class="button-oval-primary-size-2">Read More</a>
+									</div>
+									<div class="span-7 va-middle text-right">
+										<small>Posted in <a href="blog.html">Web Design</a></small>
+									</div>
+								</div>
+							</div>
+							
+							<div class="span-4 relative">	
+								<div class="thumbnail-zoom">
+									<div class="thumbnail_mask">
+										<div class="icon_group-small">
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-search"></i>
+											</a>
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-link"></i>
+											</a>
+										</div>
+									</div>
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-5.jpg">
+								</div>
+								<header class="heading_group">
+									<div class="heading_date">
+										<div>Sep</div>
+										<div>23</div>
+									</div>
+									<h3 class="heading-heavy-size-3">Kayzen is Released</h3>
+									<h4 class="heading-light">Posted by <a href="#">Naomi Olson</a></h4>
+								</header>
+								<p>Cum socis natoque penatibus et magnis dis parturient montes...</p>
+								<div class="row">
+									<div class="span-5 va-middle">
+										<a href="#" class="button-oval-primary-size-2">Read More</a>
+									</div>
+									<div class="span-7 va-middle text-right">
+										<small>Posted in <a href="blog.html">Web Design</a></small>
+									</div>
+								</div>
+							</div>
+							
+							<div class="span-4 relative">	
+								<div class="thumbnail-zoom">
+									<div class="thumbnail_mask">
+										<div class="icon_group-small">
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-search"></i>
+											</a>
+											<a href="#" class="icon-border-circle-size-3">
+												<i class="fa fa-link"></i>
+											</a>
+										</div>
+									</div>
+									<img src="<?php echo appDir ?>/images/wallpapers/bg-3.jpg">
+								</div>
+								<header class="heading_group">
+									<div class="heading_date">
+										<div>Sep</div>
+										<div>23</div>
+									</div>
+									<h3 class="heading-heavy-size-3">Kayzen is Released</h3>
+									<h4 class="heading-light">Posted by <a href="#">Naomi Olson</a></h4>
+								</header>
+								<p>Cum socis natoque penatibus et magnis dis parturient montes...</p>
+								<div class="row">
+									<div class="span-5 va-middle">
+										<a href="#" class="button-oval-primary-size-2">Read More</a>
+									</div>
+									<div class="span-7 va-middle text-right">
+										<small>Posted in <a href="blog.html">Web Design</a></small>
+									</div>
+								</div>
+							</div>
+					
+						</div>
+						
+					</div><!-- newsfeed -->
+					
+					<aside class="sidebar span-4">
+    
+                        <?php sidebar(); ?>
+						
+					</aside><!-- sidebar -->
+					
+				</div><!-- row -->
+				
+			</div><!-- container -->
+						
+		</section>
+        
+        <?php
+        
+            appFooter(); 
+        
+        ?>
+
+    </div><!-- Site Canvas -->
+
+    <?php include (ROOT.'/includes/ui-enhancements.php'); ?>
+
+    <?php include (ROOT.'/includes/scripts.php'); ?>
+
+</body>
+		
---
+++ templates/pages/further/search-results.php
@@ -0,0 +1,109 @@
+<?php 
+	include ('../../app.php');
+	// page config
+	$title = 'Search Results';
+	$tagLine = 'Aenean lobortis ante nunc urabitur at enim nisi.';
+?>
+
+<!DOCTYPE html>
+<html class="no-js">
+
+<?php include (ROOT.'/includes/head.php'); ?>
+
+<body>
+
+    <!-- Site Canvas -->
+    <div id="site-content">
+		
+		<?php 
+        
+            topbar();
+
+            appHeader(array(
+                'modifiers' => 'bar-absolute-dark-sticky'
+            ));
+
+            billboard(array(
+                'title'    => $title,
+                'tag-line' => $tagLine
+            ));
+            
+        ?>
+        
+        <section class="section-primary">
+            
+            <div class="container-small">
+                
+                <header class="heading_group-large text-center">
+                    <h3 class="heading-size-4">Didn't find what you were looking for?</h3>
+                    <form class="form text-center">
+                        <div class="form_group span-6">
+                            <button type="submit" class="icon-primary-size-4"><i class="fa fa-search"></i></button>
+                            <input type="text" class="form_input" id="newSearch" placeholder="Search for a page" required>
+                        </div>
+                    </form>
+                </header>
+                
+                <section id="free-support" class="object-large">
+                    <header class="heading_group-small">
+                        <h4 class="heading-size-4-dotted"><a href="#">Do I get free support?</a></h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed eros congue leo tincidunt mattis. Vivamus bibendum laoreet nibh nec gravida. Phasellus a efficitur est, ac lacinia ipsum. <mark>Aliquam erat volutpat</mark>. Ut sit amet tellus mauris. Phasellus sed vehicula ligula. Donec ac congue arcu. Morbi metus felis, feugiat facilisis turpis nec, vulputate sollicitudin eros...</p>
+                </section>
+                
+                <section id="free-support" class="object-large">
+                    <header class="heading_group-small">
+                        <h4 class="heading-size-4-dotted"><a href="#">Lorem Ipsum Dolor Sit Amet</a></h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed eros congue leo tincidunt mattis. <mark>Aliquam erat volutpat</mark>. Vivamus bibendum laoreet nibh nec gravida. Phasellus a efficitur est, ac lacinia ipsum. Ut sit amet tellus mauris. Phasellus sed vehicula ligula. Donec ac congue arcu. Morbi metus felis, feugiat facilisis turpis nec, vulputate sollicitudin eros...</p>
+                </section>
+                
+                <section id="free-support" class="object-large">
+                    <header class="heading_group-small">
+                        <h4 class="heading-size-4-dotted"><a href="#">About Us</a></h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed eros congue leo tincidunt mattis. Vivamus bibendum laoreet nibh nec gravida. Phasellus a efficitur est, ac lacinia ipsum. Ut sit amet tellus mauris. Phasellus sed vehicula ligula. Donec ac congue arcu. <mark>Aliquam erat volutpat</mark>. Morbi metus felis, feugiat facilisis turpis nec, vulputate sollicitudin eros...</p>
+                </section>
+                
+                <section id="free-support" class="object-large">
+                    <header class="heading_group-small">
+                        <h4 class="heading-size-4-dotted"><a href="#">Phasellus Sed Vehicula Ligula</a></h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <mark>Aliquam erat volutpat</mark>. Vivamus sed eros congue leo tincidunt mattis. Vivamus bibendum laoreet nibh nec gravida. Phasellus a efficitur est, ac lacinia ipsum. Ut sit amet tellus mauris. Phasellus sed vehicula ligula. Donec ac congue arcu. Morbi metus felis, feugiat facilisis turpis nec, vulputate sollicitudin eros...</p>
+                </section>
+                
+                <section id="free-support" class="object">
+                    <header class="heading_group-small">
+                        <h4 class="heading-size-4-dotted"><a href="#">Lorem Ipsum Dolor Sit Amet</a></h4>
+                    </header>
+                    <p><mark>Aliquam erat volutpat</mark>. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed eros congue leo tincidunt mattis. Vivamus bibendum laoreet nibh nec gravida. Phasellus a efficitur est, ac lacinia ipsum. Ut sit amet tellus mauris. Phasellus sed vehicula ligula. Donec ac congue arcu. Morbi metus felis, feugiat facilisis turpis nec, vulputate sollicitudin eros...</p>
+                </section>
+                
+                <ul class="pagination tabs_nav-pills object-center">
+                    <li class="button-pill-grey-1-thin"><i class="fa fa-angle-left"></i></li>
+                    <li class="button-pill-grey-1-thin">1</li>
+                    <li class="button-pill-grey-1-thin active">2</li>
+                    <li class="button-pill-grey-1-thin">3</li>
+                    <li class="button-pill-grey-1-thin">...</li>
+                    <li class="button-pill-grey-1-thin">10</li>
+                    <li class="button-pill-grey-1-thin"><i class="fa fa-angle-right"></i></li>
+                </ul>
+                
+            </div><!-- container -->
+            
+        </section><!-- section -->	
+        
+        <?php
+        
+            appFooter(); 
+        
+        ?>
+
+    </div><!-- Site Canvas -->
+
+    <?php include (ROOT.'/includes/ui-enhancements.php'); ?>
+
+    <?php include (ROOT.'/includes/scripts.php'); ?>
+
+</body>
+		
