8c8e832935bfcc02a942a4f34131850f00cb1fad
Edmund Reed <esr360@live.com>
2016-04-25 02:20:02 +0100

    updating preloaders.scss

--- templates/modules/preloader.php
+++ templates/modules/preloader.php
@@ -1,25 +1,27 @@
 <?php function preloader($style = 'clock') { ?>
 
     <!-- Preloader -->
-        <div class="preloader">
-            <div class="preloader_close">
-                <div class="button-round-border-size-2-thin-grey-3">Disable Preloader</div>
-            </div>
-            <?php if ($style == 'clock') { ?>
-                <div class="awd_clock">
-                    <div></div>
-                    <div></div>
-                </div>
-            <?php } else if ($style == 'pacman') { ?>
-                <div class="awd_pacman">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            <?php } ?>
+    <div class="preloader">
+        <div class="preloader_close">
+            <div class="button-round-border-size-2-thin-grey-3">Disable Preloader</div>
         </div>
+        <?php if ($style == 'clock') { ?>
+            <div class="awd_clock">
+                <div></div>
+                <div></div>
+            </div>
+        <?php } else if ($style == 'pacman') { ?>
+            <div class="awd_pacman">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        <?php } else if ($style == 'nexus') { ?>
+            <div class="spinner"></div>
+        <?php } ?>
+    </div>
     
 <?php } ?>
